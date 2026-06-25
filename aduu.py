4ea0f8e20d148a2ae483349d8590224d2a8723e6
sarveshg284-gif <sarveshghadi284@gmail.com>
2026-06-25 11:54:35 +0530

    Fix indentation in HTML content in aduu.py

--- aduu.py
+++ aduu.py
@@ -140,10 +140,13 @@ if st.session_state.show_gift:
             💐 🧸 🍫
         </div>
         st.markdown("""
-<p style='text-align:center;font-size:18px;color:#333;line-height:1.8;'>
-   I don't know how much you love me, but I love you with all my heart."❤️🤌🙂<br>
-</p>
-""", unsafe_allow_html=True)
+        <p style='text-align:center;font-size:18px;color:#333;line-height:1.8;'>
+                I don't know how much you love me, but I love you with all my heart."❤️🤌🙂<br>
+                </p>
+                """, unsafe_allow_html=True)
+
+
+   
        
         <p style="font-size:22px;color:#ff1493;">
             You are special ❤️
